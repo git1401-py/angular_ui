2be83fd0f881d267cd3147184df31afdda591057
git1401-py <quick.k1399@gmail.com>
2025-03-31 22:01:33 +0330

    add storybook for primary button

--- projects/angular-ui-lib/src/lib/buttons/buttons.module.ts
+++ projects/angular-ui-lib/src/lib/buttons/buttons.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { BaseButtonComponent } from './base-button/base-button.component';
+import { PrimaryButtonComponent } from './primary-button/primary-button.component';
+// Import other button components here...
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BaseButtonComponent,
+    PrimaryButtonComponent,
+    // Add other button components here...
+  ],
+  exports: [
+    BaseButtonComponent,
+    PrimaryButtonComponent,
+    // Export other button components here...
   ]
 })
 export class ButtonsModule { }
+
+// آماده‌سازی صادرات عمومی
+export * from './base-button/base-button.component';
+export * from './primary-button/primary-button.component';
+// Export other button components here...
